345b9c0e8a56c4f0e186db743806ed858433eb61
おーしろ <sabitetsu.a24h@gmail.com>
2021-08-13 01:00:29 +0900

    LineObjの生成
    タップした場所から離した場所までの線を引きたい。現在はパソコンの画面のマウス座標になっている。

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Shot : MonoBehaviour
 {
+    Vector3 startPos;
+    Vector3 nowPos;
+    Vector3 endPos;
+    GameObject LineObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +17,48 @@ public class Shot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //startPos = Input.mousePosition;
+            startPos = new Vector3(0, 1, 0);
+            TargetLine(startPos, startPos);
+            Debug.Log("START");
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            nowPos = Input.mousePosition;
+            nowPos.y = 1;
+            Destroy(LineObj);
+            TargetLine(startPos, nowPos);
+            Debug.Log("NOW");
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            endPos = Input.mousePosition;
+            //endPos.y = 1;
+            Destroy(LineObj);
+            TargetLine(startPos, endPos);
+            Debug.Log("END");
+        }
     }
 
     void ShotBall()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.AddForce(0, 0, 0, ForceMode.Impulse);
+    }
+
+    void TargetLine(Vector3 startVec, Vector3 endVec)
+    {
+
+        LineObj = new GameObject("Line");
+        LineRenderer lRend = LineObj.AddComponent<LineRenderer>();
+        lRend.positionCount = 2;
+        lRend.startWidth = 0.5f;
+        lRend.endWidth = 0.5f;
+        //Vector3 startVec = new Vector3(-10.0f, 10.0f, 0.0f);
+        //Vector3 endVec = new Vector3(0.0f, 0.0f, 0.0f);
+        lRend.SetPosition(0, startVec);
+        lRend.SetPosition(1, endVec);
     }
 }
